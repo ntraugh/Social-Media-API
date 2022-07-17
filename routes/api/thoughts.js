b6eff821e516c4f122884a52af1415981a0db70e
Nate Traugh <n.traugh@yahoo.com>
2022-07-17 19:51:46 -0400

    created thoughts api routes

---
+++ routes/api/thoughts.js
@@ -0,0 +1,16 @@
+const router = require("express").Router()
+
+const {
+    getAllThoughts,
+    getThoughtId,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction,
+} = require("../../controllers/thoughts-controller")
+
+router.route("/").get(getAllThoughts).post(createThought)  
+router.route("/:id").get(getThoughtId).put(updateThought).delete(deleteThought)
+
+router.route("/:thoughtId/reaction").post(addReaction).delete(deleteReaction)
